60f0af060d0f3b17ebea2d125ac94866b8001261
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-05-08 10:53:22 -0700

    testing not working

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,47 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const { getByText } = render(<CheckoutForm />);
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const { getByLabelText, getByText, getByTestId } = render(<CheckoutForm />);
+
+    const header = getByText(/checkout form/i);
+
+    expect(header).toBeInTheDocument();
+  });
+
+        const firstNameInput = getByLabelText(/first Name/i);
+        const lastNameInput = getByLabelText(/last Name/i);
+        const addressInput = getByLabelText(/address/i);
+        const cityInput = getByLabelText(/city/i);
+        const stateInput = getByLabelText(/state/i);
+        const zipInput = getByLabelText(/zip/i);
+
+    fireEvent.change(firstNameInput,{target: { value: "drake" }
+});
+    fireEvent.change(lastNameInput,{target: { value: "alia" }
+});
+    fireEvent.change(addressInput,{target: { value: "classified" }
+});
+    fireEvent.change(cityInput,{target: { value: "gotham city" }
+});
+    fireEvent.change(stateInput,{target: { value: "america" }
+});
+    fireEvent.change(zipInput,{target: { value: "batcave" }
+});
+
+const checkoutButton = getByLabelText(/checkout/i);
+
+fireEvent.click(checkoutButton);
+
+findAllByText(/drake/i);
+
+expect(getByTestId("successMessage")).toBeInTheDocument();
+
+});
--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -11,6 +11,7 @@ export default class PlantList extends Component {
     axios
     .get("http://localhost:3333/plants")
     .then(res => {
+      // this.state.plants
       this.setState({ plants: res.data.plantData });
     })
     .catch(err => console.log(err));
@@ -23,7 +24,8 @@ export default class PlantList extends Component {
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
+        {/* {this.state?.plants?.map((plant) => ( */}
+          {this.state?.plants?.map(plant => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
             <div className="plant-details">
